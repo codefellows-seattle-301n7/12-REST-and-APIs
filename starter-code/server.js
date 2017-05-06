213935b9f40756d700718b077874c2a4b1cfdee0
Maks <pvl1720@gmail.com>
2017-05-06 12:19:25 -0700

    added missing script tags in index , handlebars, added token

--- starter-code/server.js
+++ starter-code/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 // const conString = 'postgres://USERNAME:PASSWORD@HOST:PORT';
-const conString = 'postgres://pablo@localhost:5432/lab12'; // TODO: Don't forget to set your own conString
+const conString = 'postgres://maks@localhost:5432/template1'; // TODO: Don't forget to set your own conString
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.error(err));
